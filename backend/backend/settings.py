4f8440f17bacd9f86cdc55e319fe11e75c072f21
Sam Marinovich <s.marinovich@student.unsw.edu.au>
2025-10-09 10:07:12 +1100

    feat:add be template

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# The secret key
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# The secret key a dummy key :)
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
  
 DEBUG = bool(os.environ.get("DEBUG", default=0))
  
@@ -74,11 +74,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.{}'.format(
+             os.getenv('DATABASE_ENGINE', 'sqlite3')
+         ),
+         'NAME': os.getenv('DATABASE_NAME', 'polls'),
+         'USER': os.getenv('DATABASE_USERNAME', 'myprojectuser'),
+         'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
+         'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+         'PORT': os.getenv('DATABASE_PORT', 5432),
+     }
+ }
 
 
 # Password validation
