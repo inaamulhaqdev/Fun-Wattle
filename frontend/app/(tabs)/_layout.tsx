7469aa296192bfb206773722c0f3e14f3dabe28c
Angela Cai <ang3lacai@gmail.com>
2025-10-14 01:30:54 +1100

    fix: deleted extraneous tab in _layout

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -37,13 +37,6 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="learning-unit-details"
-        options={{
-          title: 'Learning Unit',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
     </Tabs>
   );
 }
