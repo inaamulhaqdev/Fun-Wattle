a3221c72c797679f9d6a7906be7a4c6f7c3fe0c0
Sam Marinovich <s.marinovich@student.unsw.edu.au>
2025-10-14 23:24:41 +1100

    feat: start views

--- backend/controller/views.py
+++ backend/controller/views.py
@@ -2,8 +2,32 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from .models import *
-from .serializers import *
+from .models import Parent, Therapist, Child, Activity, AssignedActivity
+from .serializers import ParentSerializer, TherapistSerializer, ChildSerializer, ActivitySerializer, AssignedActivitySerializer
+from rest_framework.exceptions import MethodNotAllowed
+
+class ParentViewSet(viewsets.ModelViewSet):
+	queryset = Parent.objects.all()
+	serializer_class = ParentSerializer
+
+	def destroy(self, request, *args, **kwargs):
+		raise MethodNotAllowed('DELETE')
+
+class TherapistViewSet(viewsets.ModelViewSet):
+	queryset = Therapist.objects.all()
+	serializer_class = TherapistSerializer
+
+class ChildViewSet(viewsets.ModelViewSet):
+	queryset = Child.objects.all()
+	serializer_class = ChildSerializer
+
+class ActivityViewSet(viewsets.ModelViewSet):
+	queryset = Activity.objects.all()
+	serializer_class = ActivitySerializer
+
+class AssignedActivityViewSet(viewsets.ModelViewSet):
+	queryset = AssignedActivity.objects.all()
+	serializer_class = AssignedActivitySerializer
 
 
 
