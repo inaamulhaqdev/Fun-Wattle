180be513bd4a1e85a1714e1a46d635135610f3dc
LachieJK <lk77.external@pm.me>
2025-10-15 19:24:13 +1100

    feat: more changes to new postgres schema

--- backend/controller/models.py
+++ backend/controller/models.py
@@ -1,55 +1,48 @@
 from django.db import models
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey
 
 ######################### USER MODELS #########################
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
-    firebase_auth_uid = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(unique=True)
-    USER_TYPES = [
+    firebase_auth_uid = models.CharField(unique=True, max_length=255)
+    USER_TYPES = (
         ('parent', 'Parent'),
         ('therapist', 'Therapist'),
-    ]
+    )
     user_type = models.CharField(max_length=10, choices=USER_TYPES)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.email} ({self.user_type})"
+        return f"user_type={self.user_type}, email={self.email}"
+
 
 class Profile(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    profile_picture = models.URLField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    pin_hash = models.CharField(max_length=256, blank=True, null=True)  # Optional for Therapist, required for Parent (enforced in views)
-    PROFILE_TYPES = [
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', null=True, blank=True) # For parents and therapists we can easily find their firebase user and email, it will be null for children
+    PROFILE_TYPES = (
         ('parent', 'Parent'),
         ('therapist', 'Therapist'),
         ('child', 'Child'),
-    ]
+    )
     profile_type = models.CharField(max_length=10, choices=PROFILE_TYPES)
+    name = models.CharField(max_length=100)
+    profile_picture = models.URLField(blank=True, null=True)
+    pin_hash = models.CharField(max_length=256, blank=True, null=True)  # Null for children, Optional for Therapist, required for Parent (enforced in views)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.name} ({self.profile_type})"
+        return f"name={self.name}, profile_type={self.profile_type}"
 
 
-class UserChildProfile(models.Model):
+class User_ChildProfile(models.Model):
     id = models.AutoField(primary_key=True)
-    
-    user = models.ForeignKey(
-        User,
-        on_delete=models.PROTECT,  # Temporarily PROTECT for safety. Will CASCADE if we want links to auto-delete when a user is removed.
-        related_name="linked_children"
-    )
-    
-    child_profile = models.ForeignKey(
-        Profile,
-        on_delete=models.PROTECT,  # Temporarily PROTECT for safety.
-        related_name="linked_users"
-    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='linked_children')
+    child = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='linked_users')
+
     def __str__(self):
-        return f"User {self.user.email}, Child {self.child_profile.name}"
+        return f"user={self.user.email}, child={self.child.name}"
+
 
 ######################### LEARNING ACTIVITY MODELS #########################
 
@@ -65,20 +58,15 @@ class Activity(models.Model):
 
 class AssignedActivity(models.Model):
     id = models.AutoField(primary_key=True)
-    activity = models.ForeignKey(Activity, on_delete=models.CASCADE, related_name='assigned_activities')
-    child = models.ForeignKey(Child, on_delete=models.CASCADE, related_name='assigned_activities')
-
-    # Generic foreign key fields
-    assigned_by_type = models.ForeignKey(ContentType, on_delete=models.SET_NULL, null=True) # Stores which model (Parent or Therapist) assigned the activity
-    assigned_by_id = models.PositiveIntegerField(null=True) # Stores the ID of the specific Parent or Therapist who assigned the activity
-    assigned_by = GenericForeignKey('assigned_by_type', 'assigned_by_id') # Generic relation to either Parent or Therapist
-
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE, related_name='assignments')
+    child_assigned_to = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='received_activities')
+    user_assigned_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='given_activities')
     assigned_at = models.DateTimeField(auto_now_add=True)
     due_date = models.DateTimeField(null=True, blank=True)
-    completed = models.BooleanField(default=False)
+    completed_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return f"Activity '{self.activity.title}' assigned to {self.child.name}"
+        return f"Activity '{self.activity.title}' assigned to {self.child_assigned_to.name}"
 
 
 
