648cf68d3753ebd59b45219bd2978ccaa9d1787d
LachieJK <lk77.external@pm.me>
2025-10-15 14:37:09 +1100

    feat: create_profile route works, tested using postman

--- backend/controller/urls.py
+++ backend/controller/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import register_user
+from .views import register_user, create_profile
 
 urlpatterns = [
     path('register/', register_user, name='register_user'),
+    path('create_profile/', create_profile, name='create_profile'),
 ]
--- backend/controller/views.py
+++ backend/controller/views.py
@@ -42,18 +42,12 @@ def register_user(request):
 	user_type = request.data.get('user_type')
 	firebase_auth_uid = request.data.get('firebase_auth_uid')
 	email = request.data.get('email')
-	# name = request.data.get('name')
-	# profile_picture = request.data.get('profile_picture', None)
-	# pin_hash = request.data.get('pin_hash')
 
 	if not all([user_type, firebase_auth_uid, email]):
 		return Response({'error': 'Missing required fields'}, status=400)
 
 	if user_type not in ['parent', 'therapist']:
 		return Response({'error': 'Invalid user type'}, status=400)
-
-	# if user_type == 'parent' and not pin_hash:
-	# 	return Response({'error': 'Pin hash is required for parent registration'}, status=400)
 
 	model = Parent if user_type == 'parent' else Therapist
 
@@ -68,5 +62,57 @@ def register_user(request):
 	# Must serialize model before returning it
 	serializer = ParentSerializer(user) if user_type == 'parent' else TherapistSerializer(user)
 	return Response(serializer.data, status=201)
+
+
+@api_view(['POST'])
+def create_profile(request):
+    user_type = request.data.get('user_type')
+
+    if user_type in ['parent', 'therapist']:
+        firebase_auth_uid = request.data.get('firebase_auth_uid')
+        name = request.data.get('name')
+        profile_picture = request.data.get('profile_picture')
+        pin_hash = request.data.get('pin_hash')
+        model = Parent if user_type == 'parent' else Therapist
+
+        try:
+            user = model.objects.get(firebase_auth_uid=firebase_auth_uid)
+        except model.DoesNotExist:
+            return Response({'error': 'User not found, cannot create parent / therapist profile without user login'}, status=404)
+
+        # Update fields they provided
+        if name:
+            user.name = name
+        if profile_picture:
+            user.profile_picture = profile_picture
+        if pin_hash:
+            user.pin_hash = pin_hash
+        user.save()
+
+        serializer = ParentSerializer(user) if user_type == 'parent' else TherapistSerializer(user)
+        return Response(serializer.data, status=200)
+
+    elif user_type == 'child':
+        name = request.data.get('name')
+        parent_ids = request.data.get('parent_ids', [])
+        therapist_ids = request.data.get('therapist_ids', [])
+
+        # Im assuming only at least 1 parent can create a child profile, so therapist_ids are optional
+        if not name or not parent_ids:
+            return Response({'error': 'Missing required fields for child, name and at least 1 parent_id are required'}, status=400)
+
+        child = Child.objects.create(name=name)
+
+		# This handles association tables in db (controller_child_parent and controller_child_therapist)
+        if parent_ids:
+            child.parent.set(parent_ids)
+        if therapist_ids:
+            child.therapist.set(therapist_ids)
+
+        serializer = ChildSerializer(child)
+        return Response(serializer.data, status=201)
+
+    else:
+        return Response({'error': 'Invalid user type'}, status=400)
 
 
