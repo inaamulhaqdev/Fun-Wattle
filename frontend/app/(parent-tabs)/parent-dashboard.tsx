690ae50cdd45cc076405f57028b09f59288600d8
Angela Cai <ang3lacai@gmail.com>
2025-11-15 00:00:28 +1100

    refactor: Remove redundant code

--- frontend/app/(parent-tabs)/parent-dashboard.tsx
+++ frontend/app/(parent-tabs)/parent-dashboard.tsx
@@ -145,46 +145,6 @@ export default function ParentDashboard() {
   // Get assignments on focus
   useFocusEffect(
     React.useCallback(() => {
-      const fetchAssignments = async () => {
-        try {
-          const assignmentsResp = await fetch(`${API_URL}/assignment/${userId}/assigned_by/`);
-
-          if (!assignmentsResp.ok) throw new Error('Failed to fetch data');
-
-          const assignments = await assignmentsResp.json();
-
-          const childAssignments = assignments.filter((a: any) => a.assigned_to.id === childId);
-
-          const assignedUnitsDetails: AssignedLearningUnit[] = childAssignments.map((assignment: any) => ({
-            assignmentId: assignment.id,
-            learningUnitId: assignment.learning_unit.id,
-            title: assignment.learning_unit.title || '',
-            category: assignment.learning_unit.category || '',
-            participationType: assignment.participation_type,
-            assignedDate: formatDate(assignment.assigned_at),
-          }));
-
-          const assignedUnitsWithStats: AssignedLearningUnit[] = await Promise.all(
-            assignedUnitsDetails.map(async (unit) => {
-              const { totalDuration, status } = await fetchUnitStats(unit.learningUnitId, childId);
-              return {
-                ...unit,
-                time: totalDuration,
-                status,
-              };
-            })
-          );
-
-          setData(assignedUnitsWithStats);
-
-        } catch (err) {
-          console.error(err);
-          Alert.alert('Error', 'Failed to load learning units.');
-        } finally {
-          setLoadingAssignments(false);
-        }
-      };
-
       fetchAssignments();
     }, [fetchAssignments])
   );
