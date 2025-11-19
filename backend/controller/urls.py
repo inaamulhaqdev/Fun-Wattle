29c7478de7b3f90e72f74402165f12338089f8ab
Sam Marinovich <s.marinovich@student.unsw.edu.au>
2025-11-19 11:57:51 +1100

    fix: fix route typo

--- backend/controller/urls.py
+++ backend/controller/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('profile/<str:profile_id>/mascot/', mascot, name='manage_mascot'), # GET, PUT
 
     # therapist 
-    path('therapist /', therapist, name='therapist' ), # GET, POST
+    path('therapist/', therapist, name='therapist' ), # GET, POST
 
     # Content Library Routes
     path('content/learning_units/', get_all_learning_units, name='get_all_learning_units'), # GET
